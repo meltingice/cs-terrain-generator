39be5f6520a0288ad51bd1fe740e48c9cf3bf43b
Ryan LeFevre <meltingice8917@gmail.com>
2015-04-08 22:35:41 -0400

    Add new upper/lower clip params to make it easier to generate canyons/oceans/lakes.

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var argv = require('minimist')(process.argv.slice(2), {
   alias: {
     'smoothness': 's',
     'width': 'w',
-    'height': 'h'
+    'height': 'h',
+    'lowerclip': 'l',
+    'upperclip': 'u'
   }
 });
 
@@ -23,11 +25,29 @@ if (!output) {
 // Cities: Skylines requires a 1081x1081 image.
 var width   = argv.width || 1081,
     height  = argv.height || 1081,
+    lowerClip = argv.lowerclip || 0,
+    upperClip = argv.upperclip || 0,
     smoothness = argv.smoothness || 1.0;
+
+if (lowerClip > 255 || upperClip > 255) {
+  console.log("ERROR: lower/upper clip must be between [0, 255].");
+  process.exit(1);
+}
+
+if (lowerClip + upperClip > 255) {
+  console.log("ERROR: invalid values for lower and upper clips. Upper + lower clip cannot be greater than 255.");
+  process.exit(1);
+}
+
+if (smoothness < 0.0 || smoothness > 1.0) {
+  console.log("ERROR: smoothness must be between [0.0, 1.0].");
+  process.exit(1);
+}
 
 console.log("Generating terrain...");
 console.log("Width = " + width + ", Height = " + height + ", Smoothness = " + smoothness);
+console.log("Lower clip = " + lowerClip + ", Upper clip = " + upperClip);
 
-var tg = new TerrainGenerator(width, height, smoothness);
+var tg = new TerrainGenerator(width, height, smoothness, lowerClip, upperClip);
 tg.generate();
 tg.write(output);
